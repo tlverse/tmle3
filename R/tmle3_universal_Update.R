0bf4e90e7274449ee339ac2591d1c7270d13d99d
Jeremy Coyle <jeremyrcoyle@gmail.com>
2018-09-18 11:47:22 -0700

    initial implementation

---
+++ R/tmle3_universal_Update.R
@@ -0,0 +1,168 @@
+#' Defines an update (submodel+loss function)
+#'
+#' Current Limitations:
+#' only updating one node
+#' loss function and submodel are hard-coded (need to accept arguments for these)
+#' no support for one-step (recursive TMLE)
+#' @importFrom R6 R6Class
+#'
+#' @export
+#
+tmle3_universal_Update <- R6Class(
+  classname = "tmle3_universal_Update",
+  portable = TRUE,
+  class = TRUE,
+  public = list(
+    initialize = function(maxit = 10000, delta_epsilon = 1e-5, line_search = FALSE) {
+      private$.maxit <- maxit
+      private$.delta_epsilon <- delta_epsilon
+      private$.line_search <- line_search
+    },
+    update_step = function(likelihood, tmle_task, cv_fold = -1) {
+      # cv_fold=0 -- validation sets
+      # so we estimate epsilon using valudation sets
+
+      # get new submodel fit
+      all_submodels <- self$generate_submodel_data(likelihood, tmle_task, cv_fold)
+      if(self$line_search){
+        new_epsilons <- self$fit_submodels(all_submodels)
+      } else {
+        new_epsilons <- list(rep(self$delta_epsilon, length(all_submodels)))
+        names(new_epsilons) <- names(all_submodels)
+                             
+      }
+      # update likelihoods
+      likelihood$update(new_epsilons, self$step_number, cv_fold)
+
+      # increment step count
+      private$.step_number <- private$.step_number + 1
+    },
+    collapse_covariates = function(estimates, clever_covariates){
+      ICs <- do.call(cbind, lapply(estimates, `[[`, "IC"))
+      ED <- colMeans(ICs)
+      # do this
+      # fixed depsilon
+      # maxit large
+      EDnormed <- ED/norm(ED, type = "2")
+      collapsed_covariate <- clever_covariates %*% EDnormed
+      
+      return(collapsed_covariate)
+    },
+    generate_submodel_data = function(likelihood, tmle_task, cv_fold = -1) {
+      update_nodes <- self$update_nodes
+
+      # todo: support not getting observed for case where we're applying updates instead of fitting them
+      clever_covariates <- lapply(self$tmle_params, function(tmle_param) tmle_param$clever_covariates(tmle_task, cv_fold))
+      estimates <- lapply(self$tmle_params, function(tmle_param) tmle_param$estimates(tmle_task, cv_fold))
+      
+      observed_values <- lapply(update_nodes, tmle_task$get_tmle_node, bound = TRUE)
+
+      all_submodels <- lapply(update_nodes, function(update_node) {
+        node_covariates <- lapply(clever_covariates, `[[`, update_node)
+        covariates_dt <- do.call(cbind, node_covariates)
+        collapsed_covariate <- self$collapse_covariates(estimates, covariates_dt)
+        observed <- tmle_task$get_tmle_node(update_node, bound = TRUE)
+        initial <- likelihood$get_likelihood(tmle_task, update_node, cv_fold)
+        submodel_data <- list(
+          observed = observed,
+          H = collapsed_covariate,
+          initial = initial
+        )
+      })
+
+      names(all_submodels) <- update_nodes
+
+      return(all_submodels)
+    },
+    fit_submodel = function(submodel_data) {
+      suppressWarnings({
+        submodel_fit <- glm(observed ~ H - 1, submodel_data, offset = qlogis(submodel_data$initial), family = binomial())
+      })
+      epsilon <- coef(submodel_fit)
+
+      # this protects against collinear covariates (which we don't care about, we just want an update)
+      epsilon[is.na(epsilon)] <- 0
+      return(epsilon)
+    },
+    fit_submodels = function(all_submodels) {
+      all_epsilon <- lapply(all_submodels, self$fit_submodel)
+
+      names(all_epsilon) <- names(all_submodels)
+      private$.epsilons <- c(private$.epsilons, list(all_epsilon))
+
+      return(all_epsilon)
+    },
+    submodel = function(epsilon, initial, H) {
+      plogis(qlogis(initial) + H %*% epsilon)
+    },
+    loss_function = function(estimate, observed) {
+      -1 * ifelse(observed == 1, log(estimate), log(1 - estimate))
+    },
+    apply_submodel = function(submodel_data, epsilon) {
+      self$submodel(epsilon, submodel_data$initial, submodel_data$H)
+    },
+    apply_submodels = function(all_submodels, all_epsilon) {
+      update_nodes <- self$update_nodes
+      updated_likelihood <- mapply(self$apply_submodel, all_submodels, all_epsilon, SIMPLIFY = FALSE)
+      return(updated_likelihood)
+    },
+    check_convergence = function(tmle_task) {
+      ED_criterion <- 1 / tmle_task$nrow
+      estimates <- lapply(
+        self$tmle_params,
+        function(tmle_param) {
+          tmle_param$estimates(tmle_task)
+        }
+      )
+      ICs <- do.call(cbind, lapply(estimates, `[[`, "IC"))
+      ED <- colMeans(ICs)
+      print(max(abs(ED)))
+      return(max(abs(ED)) < ED_criterion)
+    },
+    update = function(likelihood, tmle_task) {
+      maxit <- private$.maxit
+      for (steps in seq_len(maxit)) {
+        self$update_step(likelihood, tmle_task)
+        if (self$check_convergence(tmle_task)) {
+          break
+        }
+      }
+    }
+  ),
+  active = list(
+    epsilons = function() {
+      return(private$.epsilons)
+    },
+    tmle_params = function(new_params = NULL) {
+      if (!is.null(new_params)) {
+        if (inherits(new_params, "Param_base")) {
+          new_params <- list(new_params)
+        }
+        private$.tmle_params <- new_params
+        private$.update_nodes <- unique(unlist(lapply(new_params, `[[`, "update_nodes")))
+      }
+      return(private$.tmle_params)
+    },
+    update_nodes = function() {
+      return(private$.update_nodes)
+    },
+    step_number = function() {
+      return(private$.step_number)
+    },
+    delta_epsilon = function(){
+      return(private$.delta_epsilon)
+    },
+    line_search = function(){
+      return(private$.line_search)
+    }
+  ),
+  private = list(
+    .epsilons = list(),
+    .delta_epsilon = NULL,
+    .tmle_params = NULL,
+    .update_nodes = NULL,
+    .step_number = 0,
+    .maxit = 100,
+    .line_search = FALSE
+  )
+)
---
+++ tests/testthat/test-universal_submodel.R
@@ -0,0 +1,113 @@
+context("universal submodel")
+
+library(sl3)
+# library(tmle3)
+library(uuid)
+library(assertthat)
+library(data.table)
+library(future)
+# setup data for test
+data(cpp)
+data <- as.data.table(cpp)
+data$parity01 <- as.numeric(data$parity > 0)
+data$parity01_fac <- factor(data$parity01)
+data$haz01 <- as.numeric(data$haz > 0)
+data[is.na(data)] <- 0
+node_list <- list(
+  W = c(
+    "apgar1", "apgar5", "gagebrth", "mage",
+    "meducyrs", "sexn"
+  ),
+  A = "parity01",
+  Y = "haz01"
+)
+
+qlib <- make_learner_stack(
+  "Lrnr_mean",
+  "Lrnr_glm_fast"
+)
+
+glib <- make_learner_stack(
+  "Lrnr_mean",
+  "Lrnr_glm_fast"
+)
+
+logit_metalearner <- make_learner(Lrnr_solnp, metalearner_logistic_binomial, loss_loglik_binomial)
+Q_learner <- make_learner(Lrnr_sl, qlib, logit_metalearner)
+g_learner <- make_learner(Lrnr_sl, glib, logit_metalearner)
+learner_list <- list(Y = Q_learner, A = g_learner)
+tmle_spec <- tmle_TSM_all()
+
+# define data
+tmle_task <- tmle_spec$make_tmle_task(data, node_list)
+
+# define likelihood
+initial_likelihood <- tmle_spec$make_initial_likelihood(tmle_task, learner_list)
+
+# define update method (submodel + loss function)
+updater <- tmle3_universal_Update$new(line_search=FALSE)
+
+targeted_likelihood <- Targeted_Likelihood$new(initial_likelihood, updater)
+intervention <- define_lf(LF_static, "A", value = 1)
+params <- tmle_spec$make_params(tmle_task, targeted_likelihood)
+updater$tmle_params <- params
+
+# debug(targeted_likelihood$cache$set_values)
+# debugonce(updater$update_step)
+# debugonce(targeted_likelihood$update)
+# debugonce(updater$apply_submodel)
+Rprof(tmp <- tempfile())
+tmle_fit <- fit_tmle3(tmle_task, targeted_likelihood, params, updater)
+Rprof()
+summaryRprof(tmp)
+unlink(tmp)
+
+
+
+# extract results
+tmle3_psi <- tmle_fit$summary$tmle_est
+tmle3_se <- tmle_fit$summary$se
+tmle3_epsilon <- updater$epsilons[[1]]$Y
+
+submodel_data <- updater$generate_submodel_data(initial_likelihood, tmle_task, -1)
+#################################################
+# compare with the tmle package
+library(tmle)
+
+# construct likelihood estimates
+
+# task for A=1
+# cf_task <- tmle_task$generate_counterfactual_task(UUIDgenerate(), data.table(A = 1))
+cf_task <- tsm$cf_likelihood$cf_tasks[[1]]
+
+# get Q
+EY1 <- initial_likelihood$get_likelihoods(cf_task, "Y")
+EY1_final <- targeted_likelihood$get_likelihoods(cf_task, "Y")
+EY0 <- rep(0, length(EY1)) # not used
+Q <- cbind(EY0, EY1)
+
+# get G
+pA1 <- initial_likelihood$get_likelihoods(cf_task, "A")
+pDelta1 <- cbind(pA1, pA1)
+tmle_classic_fit <- tmle(
+  Y = tmle_task$get_tmle_node("Y"),
+  A = NULL,
+  W = tmle_task$get_tmle_node("W"),
+  Delta = tmle_task$get_tmle_node("A"),
+  Q = Q,
+  pDelta1 = pDelta1
+)
+
+cf_task <- tsm$cf_likelihood$cf_tasks[[1]]
+# debugonce(tsm$cf_likelihood$enumerate_cf_tasks)
+
+# extract estimates
+classic_psi <- tmle_classic_fit$estimates$EY1$psi
+classic_se <- sqrt(tmle_classic_fit$estimates$EY1$var.psi)
+classic_epsilon <- tmle_classic_fit$epsilon[["H1W"]]
+classic_Qstar <- tmle_classic_fit$Qstar[, 2]
+
+test_that("Qstar matches result from classic package", expect_equivalent(EY1_final, classic_Qstar))
+test_that("psi matches result from classic package", expect_equal(tmle3_psi, classic_psi))
+test_that("se matches result from classic package", expect_equal(tmle3_se, classic_se))
+test_that("epsilon matches resullt from classic package", expect_equivalent(tmle3_epsilon, classic_epsilon))

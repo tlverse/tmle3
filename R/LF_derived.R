6c75134827e586e4f6e626efa13bee9f3a6265ef
Jeremy Coyle <jeremyrcoyle@gmail.com>
2019-04-24 12:51:02 -0700

    add derived likelihood factor

---
+++ R/LF_derived.R
@@ -0,0 +1,98 @@
+#' Derived Likelihood Factor Estimated from Data + Other Likelihood values, using sl3.
+#'
+#' Uses an \code{sl3} learner to estimate a likelihood factor from data.
+#' Inherits from \code{\link{LF_base}}; see that page for documentation on likelihood factors in general.
+#'
+#' @importFrom R6 R6Class
+#' @importFrom uuid UUIDgenerate
+#' @importFrom methods is
+#' @family Likelihood objects
+#' @keywords data
+#'
+#' @return \code{LF_base} object
+#'
+#' @format \code{\link{R6Class}} object.
+#'
+#' @section Constructor:
+#'   \code{define_lf(LF_fit, name, learner, ..., type = "density")}
+#'
+#'   \describe{
+#'     \item{\code{name}}{character, the name of the factor. Should match a node name in the nodes specified by \code{\link{tmle3_Task}$npsem}
+#'     }
+#'     \item{\code{learner}}{An sl3 learner to be used to estimate the factor
+#'     }
+#'     \item{\code{...}}{Not currently used.
+#'     }
+#'     \item{\code{type}}{character, either "density", for conditional density or, "mean" for conditional mean
+#'     }
+#'     }
+#'
+#' @section Fields:
+#' \describe{
+#'     \item{\code{learner}}{The learner or learner fit object}
+#'     }
+#'
+#' @export
+LF_derived <- R6Class(
+  classname = "LF_derived",
+  portable = TRUE,
+  class = TRUE,
+  inherit = LF_base,
+  public = list(
+    initialize = function(name, learner, base_likelihood, task_generator, ..., type = "density") {
+      super$initialize(name, ..., type = type)
+      private$.learner <- learner
+      private$.base_likelihood <- base_likelihood
+      private$.task_generator <- task_generator
+    },
+    delayed_train = function(tmle_task) {
+      # call task generator
+      derived_task <- self$task_generator(tmle_task, self$base_likelihood)
+      
+      # just return prefit learner if that's what we have
+      # otherwise, make a delayed fit and return that
+      if (self$learner$is_trained) {
+        return(self$learner)
+      }
+
+      outcome_node <- self$name
+      learner_fit <- delayed_learner_train(self$learner, derived_task)
+      return(learner_fit)
+    },
+    train = function(tmle_task, learner_fit) {
+      super$train(tmle_task)
+      private$.learner <- learner_fit
+    },
+    get_mean = function(tmle_task, fold_number) {
+      derived_task <- self$task_generator(tmle_task, self$base_likelihood)
+      learner <- self$learner
+      preds <- learner$predict_fold(derived_task, fold_number)
+
+      return(preds)
+    },
+    get_density = function(tmle_task, fold_number) {
+      derived_task <- self$task_generator(tmle_task, self$base_likelihood)
+      learner <- self$learner
+      preds <- learner$predict_fold(derived_task, fold_number)
+      # todo: think about derived task with other outcome types (this assumes continuous)
+      return(preds)
+    }
+  ),
+  active = list(
+    learner = function() {
+      return(private$.learner)
+    },
+    task_generator = function(){
+      return(private$.task_generator)
+    },
+    base_likelihood = function(){
+      return(private$.base_likelihood)
+    }
+  ),
+  private = list(
+    .name = NULL,
+    .learner = NULL,
+    .base_likelihood = NULL,
+    .task_generator = NULL
+  )
+)
---
+++ tests/testthat/test-lf_derived.R
@@ -0,0 +1,116 @@
+context("LF_derived: Derived Likelihood factors")
+
+context("TML estimator for incremental propensity score interventions")
+
+library(data.table)
+library(stringr)
+library(future)
+library(hal9001)
+library(sl3)
+library(tmle3)
+set.seed(7128816)
+delta <- 0.5
+
+################################################################################
+# setup learners for the nuisance parameters
+################################################################################
+
+# instantiate some learners
+mean_lrnr <- Lrnr_mean$new()
+fglm_contin_lrnr <- Lrnr_glm_fast$new()
+fglm_binary_lrnr <- Lrnr_glm_fast$new(family = binomial())
+hal_contin_lrnr <- Lrnr_hal9001$new(
+  fit_type = "glmnet", n_folds = 5
+)
+hal_binary_lrnr <- Lrnr_hal9001$new(
+  fit_type = "glmnet", n_folds = 5,
+  family = "binomial"
+)
+
+################################################################################
+# setup data and simulate to test with estimators
+################################################################################
+make_simulated_data <- function(n_obs = 1000, # no. observations
+                                n_w = 3, # no. baseline covariates
+                                delta = 0.5) { # shift parameter value
+  
+  # baseline covariate -- simple, binary
+  W_1 <- rbinom(n_obs, 1, prob = 0.50)
+  W_2 <- rbinom(n_obs, 1, prob = 0.65)
+  W_3 <- rbinom(n_obs, 1, prob = 0.35)
+  W <- cbind(W_1, W_2, W_3)
+  
+  # create treatment based on baseline W
+  A <- as.numeric(rbinom(n_obs, 1, prob = (rowSums(W) / 4 + 0.1)))
+  
+  # mediators to affect the outcome
+  ## 1st mediator (binary)
+  z1_prob <- 1 - plogis((A^2 + W[, 1]) / (A + W[, 1]^3 + 0.5))
+  Z_1 <- rbinom(n_obs, 1, prob = z1_prob)
+  ## 2nd mediator (binary)
+  z2_prob <- plogis((A - 1)^3 + W[, 2] / (W[, 3] + 3))
+  Z_2 <- rbinom(n_obs, 1, prob = z2_prob)
+  ## 3rd mediator (binary)
+  z3_prob <- plogis((A - 1)^2 + 2 * W[, 1]^3 - 1 / (2 * W[, 1] + 0.5))
+  Z_3 <- rbinom(n_obs, 1, prob = z3_prob)
+  ## build matrix of mediators
+  Z <- cbind(Z_1, Z_2, Z_3)
+  
+  # create outcome as a linear function of A, W + white noise
+  Y <- Z_1 + Z_2 - Z_3 + A - 0.1 * rowSums(W)^2 +
+    rnorm(n_obs, mean = 0, sd = 0.5)
+  
+  # full data structure
+  data <- as.data.table(cbind(Y, Z, A, W))
+  setnames(data, c(
+    "Y", paste("Z", 1:3, sep = "_"), "A",
+    paste("W", seq_len(dim(W)[2]), sep = "_")
+  ))
+  return(data)
+}
+
+# get data and column names for sl3 tasks (for convenience)
+data <- make_simulated_data()
+z_names <- colnames(data)[str_detect(colnames(data), "Z")]
+w_names <- colnames(data)[str_detect(colnames(data), "W")]
+
+# set up TMLE components: NPSEM, likelihood
+npsem <- list(
+  define_node("W", c(
+    "W_1", "W_2", "W_3"
+  )),
+  define_node("A", c("A"), c("W")),
+  define_node("Z", c("Z_1", "Z_2", "Z_3"), c("A", "W")),
+  define_node("Y", c("Y"), c("Z", "A", "W"))
+)
+
+factor_list <- list(
+  define_lf(LF_emp, "W"),
+  define_lf(LF_fit, "A", hal_binary_lrnr),
+  define_lf(LF_fit, "Y", hal_contin_lrnr, type = "mean")
+)
+
+# create TMLE task
+tmle_task <- tmle3_Task$new(data, npsem = npsem)
+likelihood_def <- Likelihood$new(factor_list)
+likelihood_init <- likelihood_def$train(tmle_task)
+likelihood_init$get_likelihoods(tmle_task)
+
+# NEXT, need targeted_likelihood constructor
+updater <- tmle3_Update$new(cvtmle = FALSE)
+likelihood_targeted <- Targeted_Likelihood$new(likelihood_init, updater)
+
+make_e_task <- function(tmle_task, likelihood){
+  e_data <- tmle_task$internal_data
+  e_task <- sl3_Task$new(data = e_data,
+                         outcome = tmle_task$npsem[["A"]]$variables,
+                         covariates = c(tmle_task$npsem[["Z"]]$variables,
+                                        tmle_task$npsem[["W"]]$variables))
+  return(e_task)
+}
+
+
+lf_e <- define_lf(LF_derived, "e", hal_contin_lrnr, likelihood_targeted, make_e_task)
+lf_e_learn_fit <- lf_e$delayed_train(tmle_task)$compute()
+lf_e$train(tmle_task, lf_e_learn_fit)
+lf_e$get_likelihood(tmle_task)

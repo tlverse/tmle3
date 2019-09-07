3ac0d6394c6ed91fdea35ba8cda073f456348c24
Jeremy Coyle <jeremyrcoyle@gmail.com>
2019-09-07 13:31:55 -0700

    add support for OR via the delta method

---
+++ R/tmle3_Spec_OR.R
@@ -0,0 +1,64 @@
+#' Defines a TML Estimator for the Odds Ratio
+#'
+#' Current limitations:
+#' pretty much tailored to Param_TSM
+#' see TODOs for places generalization can be added
+#'
+#' @importFrom R6 R6Class
+#'
+#' @export
+#
+tmle3_Spec_OR <- R6Class(
+  classname = "tmle3_Spec_OR",
+  portable = TRUE,
+  class = TRUE,
+  inherit = tmle3_Spec,
+  public = list(
+    initialize = function(baseline_level = 0, contrast = 1, ...) {
+      # TODO: use sl3 param grabbing code
+      options <- list(
+        baseline_level = baseline_level,
+        contrast_level = contrast
+      )
+      do.call(super$initialize, options)
+    },
+    make_params = function(tmle_task, likelihood) {
+      baseline_level <- self$options$baseline_level
+      contrast_level <- self$options$contrast_level
+
+      intervention_base <- define_lf(LF_static, "A", value = baseline_level)
+      intervention_cont <- define_lf(LF_static, "A", value = contrast_level)
+
+      tsm_base <- Param_TSM$new(likelihood, intervention_base)
+      tsm_cont <- Param_TSM$new(likelihood, intervention_cont)
+      or <- Param_delta$new(
+        likelihood, delta_param_OR,
+        list(tsm_base, tsm_cont)
+      )
+      tmle_params <- list(tsm_base, tsm_cont, or)
+
+      return(tmle_params)
+    }
+  ),
+  active = list(),
+  private = list()
+)
+
+#' Odds Ratio
+#'
+#' O = (W, A, Y)
+#' W = Covariates
+#' A = Treatment (binary or categorical)
+#' Y = Outcome (binary or bounded continuous)
+#'
+#' @importFrom sl3 make_learner Lrnr_mean
+#'
+#' @param baseline_level The baseline risk group.
+#' @param contrast_level The contrast risk group.
+#'
+#' @export
+#
+tmle_OR <- function(baseline_level, contrast_level) {
+  # TODO: unclear why this has to be in a factory function
+  tmle3_Spec_OR$new(baseline_level, contrast_level)
+}
---
+++ tests/testthat/test-OR.R
@@ -0,0 +1,93 @@
+context("Odds Ratios")
+
+library(sl3)
+library(uuid)
+library(assertthat)
+library(data.table)
+library(future)
+set.seed(1234)
+
+# setup data for test
+data(cpp)
+data <- as.data.table(cpp)
+data$parity01 <- as.numeric(data$parity > 0)
+data$parity01_fac <- factor(data$parity01)
+data$haz01 <- as.numeric(data$haz > 0)
+data[is.na(data)] <- 0
+node_list <- list(
+  W = c(
+    "apgar1", "apgar5", "gagebrth", "mage",
+    "meducyrs", "sexn"
+  ),
+  A = "parity01",
+  Y = "haz01"
+)
+
+# metalearner <- make_learner(Lrnr_nnls)
+Q_learner <- make_learner(Lrnr_glm_fast)
+g_learner <- make_learner(Lrnr_glm_fast)
+learner_list <- list(Y = Q_learner, A = g_learner)
+tmle_spec <- tmle_OR(baseline = 0, contrast = 1)
+
+# define data
+tmle_task <- tmle_spec$make_tmle_task(data, node_list)
+
+# define likelihood
+initial_likelihood <- tmle_spec$make_initial_likelihood(tmle_task, learner_list)
+
+# define update method (submodel + loss function)
+updater <- tmle3_Update$new(cvtmle = FALSE, convergence_type = "sample_size")
+
+targeted_likelihood <- Targeted_Likelihood$new(initial_likelihood, updater)
+
+# define param
+tmle_params <- tmle_spec$make_params(tmle_task, targeted_likelihood)
+updater$tmle_params <- tmle_params
+
+# fit tmle update
+tmle_fit <- fit_tmle3(tmle_task, targeted_likelihood, tmle_params, updater)
+
+# extract results
+summary <- tmle_fit$summary
+tmle3_psi <- tmle_fit$summary$psi_transformed[[3]]
+tmle3_se <- tmle_fit$summary$se[[3]]
+#################################################
+# compare with the tmle package
+library(tmle)
+
+# construct likelihood estimates
+
+# tasks for A=1 and A=0
+cf_task1 <- tmle_params[[2]]$cf_likelihood$cf_tasks[[1]]
+cf_task0 <- tmle_params[[1]]$cf_likelihood$cf_tasks[[1]]
+
+# get Q
+EY1 <- initial_likelihood$get_likelihoods(cf_task1, "Y")
+EY0 <- initial_likelihood$get_likelihoods(cf_task0, "Y")
+EY1_final <- targeted_likelihood$get_likelihoods(cf_task1, "Y")
+EY0_final <- targeted_likelihood$get_likelihoods(cf_task0, "Y")
+Q <- cbind(EY0, EY1)
+
+# get G
+pA1 <- initial_likelihood$get_likelihoods(cf_task1, "A")
+tmle_classic_fit <- tmle(
+  Y = tmle_task$get_tmle_node("Y"),
+  A = tmle_task$get_tmle_node("A"),
+  W = cbind(tmle_task$get_tmle_node("W"), tmle_task$get_tmle_node("W")),
+  Q = Q,
+  g1W = pA1,
+  family="binomial"
+)
+
+
+# extract estimates
+classic_psi <- tmle_classic_fit$estimates$OR$psi
+classic_se <- sqrt(tmle_classic_fit$estimates$OR$var.log.psi)
+tol <- 1 / sqrt(tmle_task$nrow)
+test_that("psi matches result from classic package", {
+  expect_equal(tmle3_psi, classic_psi, tol = tol)
+})
+
+test_that("se matches result from classic package", {
+  expect_equal(tmle3_se, classic_se, tol = tol)
+})

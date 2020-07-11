4a01524a9790e4856771cd402dcaa3245fb70576
JiangHugh <JiangHugh@JiangHughs-MacBook-Pro.local>
2020-07-11 12:25:09 -0700

    survival_spec_rd

--- R/tmle3_Spec_survival.R
+++ R/tmle3_Spec_survival.R
@@ -23,6 +23,41 @@ tmle3_Spec_survival <- R6Class(
         ...
       )
     },
+
+    # TODO: check
+    transform_data = function(data, node_list) {
+      T_tilde_name <- node_list$T_tilde
+      Delta_name <- node_list$Delta
+      T_tilde_data <- data[T_tilde_name]
+      Delta_data <- data[Delta_name]
+      k_grid <- 1:max(T_tilde_data)
+
+      if (is.null(node_list$id)) {
+        id <- 1:nrow(data)
+        data <- cbind(id=id, data)
+        node_list$id <- "id"
+      }
+
+      all_times <- lapply(k_grid, function(t_current){
+        df_time <- copy(data)
+        # TODO: check
+        df_time$N <- as.numeric(t_current == T_tilde_data & Delta_data == 1)
+        df_time$A_c <- as.numeric(t_current == T_tilde_data & Delta_data == 0)
+        df_time$pre_failure <- as.numeric(t_current <= T_tilde_data)
+        df_time$t <- t_current
+        return(df_time)
+      })
+      df_long <- rbindlist(all_times)
+
+      long_node_list <- copy(node_list)
+      long_node_list$time <- "t" 
+      long_node_list$N <- "N" 
+      long_node_list$A_c <- "A_c"
+      long_node_list$pre_failure <- "pre_failure"
+
+      return(list(long_data=df_long, long_node_list=long_node_list))
+    },
+
     make_tmle_task = function(data, node_list, ...) {
       variable_types <- self$options$variable_types
       
@@ -60,6 +95,7 @@ tmle3_Spec_survival <- R6Class(
 #' @importFrom sl3 make_learner Lrnr_mean
 #' @param treatment_level the level of A that corresponds to treatment
 #' @param control_level the level of A that corresponds to a control or reference level
+#' @param target_times the time points to be targeted at during the TMLE adjustment
 #' @export
 # TODO: check variable types
 tmle_survival <- function(treatment_level, control_level, target_times = NULL, variable_types = NULL) {

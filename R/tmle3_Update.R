ba79a44d987975cc7ac4843ccfc96d6fe943beb3
Lars van der Laan <larsvanderlaan@berkeley.edu>
2020-09-27 17:33:49 -0700

    updater fix

--- R/tmle3_Update.R
+++ R/tmle3_Update.R
@@ -312,6 +312,9 @@ tmle3_Update <- R6Class(
           if(is.list(delta_epsilon)) {
             delta_epsilon <- delta_epsilon[[update_node]]
           }
+          if(is.function(delta_epsilon)) {
+            delta_epsilon <- delta_epsilon(submodel_data$initial)
+          }
           delta_epsilon <- c(0,self$delta_epsilon)
 
           min_eps = min(delta_epsilon)

01b111640932034b0192a3e95f3124a1476d088c
Lars van der Laan <Vanderlaanlars@yahoo.com>
2021-09-05 22:58:31 -0700

    Delete submodels_semiparametric.R

--- R/submodels_semiparametric.R
+++
@@ -1,9 +0,0 @@
-
-
-submodel_function <- function(eps, offset, X, observed) {
-  offset_unpacked <- sl3::unpack_predictions(offset)
-  Q0 <- offset_unpacked[[1]]
-  Q1 <- offset_unpacked[[2]]
-  Q <- offset_unpacked[[3]]
-
-}
